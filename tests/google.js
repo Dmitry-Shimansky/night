42236c25f505f1431a045872f3ec1d3d7e2de7c7
Dmitry_Shimansky <Shimansky.Dmitry@gmail.com>
2021-05-15 17:13:56 +0300

    Page Object model

--- tests/google.js
+++ tests/google.js
@@ -8,30 +8,26 @@ module.exports = {
 
     '@tags': ['google'],
     'Google advanced search: Elon Musk': function (browser) {
-
+        const date = new Date();
         const mainQuery = 'Elon Musk';
-        const mainQueryInputSelector = 'input[name="as_q"]';
-        const languageDropdownOpenerSelector = '#lr_button';
-        const languageDropdownValueSelector = '.goog-menuitem[value="lang_it"]';
-        const lastUpdateDropdownOpenerSelector = '#as_qdr_button';
-        const lastUpdateDropdownValueSelector = '.goog-menuitem[value="m"]';
-        const submitButtonSelector = '.jfk-button[type="submit"]';
+
+        const page = browser.page.googleAdvancedSearch();
+
+        page
+            .navigate()
+            .setQuery(mainQuery)
+            .selectFilter('@languageDropdown', 'lang_it')
+            .selectFilter('@lastUpdateDropdown', 'm')
+            .search()
+            .perform(() => {
+                debugger
+            });
+
         const resultsPageQuerySelector = `#searchform input[name="q"][value="${mainQuery}"]`;
         const resultPageLanguageSelector = '#hdtbMenus span:nth-child(2) div[class^="KTB"]';
         const resultPageLastUpdateSelector = '#hdtbMenus span:nth-child(3) div[class^="KTB"]';
-        const date = new Date();
 
         browser
-            .url('https://www.google.com/advanced_search')
-            .setValue(mainQueryInputSelector, mainQuery)
-            .click(languageDropdownOpenerSelector)
-            .click(languageDropdownValueSelector)
-            .click(lastUpdateDropdownOpenerSelector)
-            .click(lastUpdateDropdownValueSelector)
-            .perform(() => {
-                debugger
-            })
-            .click(submitButtonSelector)
             .assert.urlContains('as_q=Elon+Musk', 'Query is Elon Musk')
             .assert.urlContains('lr=lang_it', 'Language is Italian')
             .assert.urlContains('as_qdr=m', 'Time period is last month')
